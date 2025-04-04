096dda4b9449352c04c7adc272d71a5c6f713a60
fediksibi1996 <fediksi@outlook.com>
2025-04-04 12:34:43 +0200

    second commit

---
+++ javascript.js
@@ -0,0 +1,31 @@
+let firstNumber = "noNumber";
+let secondNumber = "noNumber";
+let displayNumber = 0;
+let buttonValue = 0;
+let currentValue = 0;
+let mathFunction = "";
+let answer = 0;
+let clearDisplay = -1;
+let sum = 0
+let displayValue = 0;
+let includesDecimal = "";
+
+function add(firstNumber,secondNumber) {
+    sum = firstNumber+secondNumber;
+}
+
+function subtract(firstNumber,secondNumber) {
+    sum = firstNumber-secondNumber;
+}
+
+function multiply(firstNumber,secondNumber) {
+    sum = firstNumber*secondNumber;
+}
+
+function divide(firstNumber,secondNumber) {
+    sum = firstNumber / secondNumber;
+}
+
+function clearTotal() {
+    sum === 0;
+}
